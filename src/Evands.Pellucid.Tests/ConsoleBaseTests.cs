4dae48916559decbc380e9c234cfb7c14c7bbec8
ProfessorAire <ProfessorAire@users.noreply.github.com>
2021-04-14 15:57:36 -0700

    feat: Adjusted header to no longer prefix program slot and added an optional header property.

---
+++ src/Evands.Pellucid.Tests/ConsoleBaseTests.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Evands.Pellucid
+{
+    /// <summary>
+    /// Summary description for ConsoleBaseTests
+    /// </summary>
+    [TestClass]
+    public class ConsoleBaseTests
+    {
+        private class TestConsoleWriter : Evands.Pellucid.Terminal.IConsoleWriter
+        {
+            public TestConsoleWriter()
+            {
+                Messages = new List<string>();
+            }
+
+            public List<string> Messages { get; set; }
+
+            public void Write(string message, params object[] args)
+            {
+                if (args != null && args.Length > 0)
+                {
+                    Messages.Add(string.Format(message, args));
+                }
+                else
+                {
+                    Messages.Add(message);
+                }
+            }
+
+            public void WriteLine(string message, params object[] args)
+            {
+                if (args != null && args.Length > 0)
+                {
+                    Messages.Add(string.Format("{0}\r\n", string.Format(message, args)));
+                }
+                else
+                {
+                    Messages.Add(string.Format("{0}\r\n", message));
+                }
+            }
+
+            public void WriteLine()
+            {
+                Messages.Add("\r\n");
+            }
+
+            public void WriteCommandResponse(string message, params object[] args)
+            {
+                this.Write(message, args);
+            }
+        }
+
+        private TestConsoleWriter writer = new TestConsoleWriter();
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            ConsoleBase.RegisterConsoleWriter(writer);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            writer.Messages.Clear();
+            ConsoleBase.UnregisterConsoleWriter(writer);
+            ConsoleBase.OptionalHeader = string.Empty;
+        }
+
+        public ConsoleBaseTests()
+        {
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void HeaderTextIsEmptyByDefault()
+        {
+            Assert.IsTrue(string.IsNullOrEmpty(ConsoleBase.OptionalHeader));
+        }
+
+        [TestMethod]
+        public void HeaderText_GetSet_Functions()
+        {
+            var expected = "01";
+            ConsoleBase.OptionalHeader = expected;
+            Assert.IsTrue(ConsoleBase.OptionalHeader == string.Format("[{0}]", expected));
+        }
+
+        [TestMethod]
+        public void WriteLine_WithEmptyHeader_WritesLineWithNoHeader()
+        {
+            var expectedStart = "[01]";
+            var expectedEnd = "Test Message";
+            ConsoleBase.WriteLine(expectedEnd);
+            var msg = writer.Messages.Last();
+            Assert.IsTrue(!msg.StartsWith(expectedStart) && msg.Contains(expectedEnd));
+        }
+
+        [TestMethod]
+        public void WriteLine_WithNonEmptyHeader_WritesLineWithHeader()
+        {
+            ConsoleBase.OptionalHeader = "01";
+            var expectedStart = "[01]";
+            var expectedEnd = "Test Message";
+            ConsoleBase.WriteLine(expectedEnd);
+            var msg = writer.Messages.Last();
+            Assert.IsTrue(msg.StartsWith(expectedStart) && msg.Contains(expectedEnd));
+        }
+    }
+}
