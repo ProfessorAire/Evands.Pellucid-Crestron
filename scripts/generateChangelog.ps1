274a0d90bcc7d330c1a11427dfafaa2abf252ba6
ProfessorAire <ProfessorAire@users.noreply.github.com>
2021-06-14 20:00:59 -0700

    chore: Created script to generate changelog.

---
+++ scripts/generateChangelog.ps1
@@ -0,0 +1,134 @@
+param(
+    [Parameter(Mandatory = $false)][switch]$IncludeOtherChanges
+)
+
+$tags = git tag --list 'v*.*.*' -i
+$tag = $tags[$tags.Length - 1]
+Write-Host "Found the tag '$tag'"
+
+$filter = "$tag..HEAD"
+
+$gitlogs = (git log $filter --format="%ai`t%H`t%an`t%ae`t%s`t%s") | ConvertFrom-Csv -Delimiter "`t" -Header ("Date", "CommitId", "Author", "Email", "Subject")
+
+$count = $gitlogs.Length;
+
+Write-Host "Found $count log items."
+
+$fixes = [System.Collections.ArrayList]@()
+$feats = [System.Collections.ArrayList]@()
+$breaks = [System.Collections.ArrayList]@()
+$others = [System.Collections.ArrayList]@()
+
+$pattern = "^(?<Type>[A-Za-z0-9]+) {0,1}\({0,1}(?<Scope>[^)]*)\){0,1}(?<Break>!{0,1}): (?<Msg>[\s\S]+)$"
+
+foreach ($item in $gitlogs)
+{
+    if ($item.Subject -match $pattern)
+    {
+        $type = $Matches.Type.ToLower()
+        if ($type -eq "fix" -or $type -eq "bug")
+        {
+            $count = $fixes.Add($Matches)
+        }
+        elseif ($type -eq "feat" -or $type -eq "feature")
+        {
+            $count = $feats.Add($Matches)
+        }
+        elseif ($Matches.Break -eq "!" -or $Matches.Msg -contains "BREAKING CHANGE:")
+        {
+            $count = $breaks.Add($Matches)
+        }
+        elseif ($IncludeOtherChanges -eq $true)
+        {
+            $count = $others.Add($Matches)
+        }
+    }
+}
+
+function WriteContent
+{
+    param(
+        [Parameter()][string]$Type,
+        [Parameter()][string]$Scope,
+        [Parameter()][string]$Message
+    )
+
+    $document = ""
+    Write-Host "$Type($Scope): $Message"
+    if ($Scope -ne "")
+    {
+        $document += "($Scope`): "
+    }
+
+    $document += "$Message`r`n"
+    "* $document"
+}
+
+$document = ""
+
+if ($breaks.Length -gt 0)
+{
+    Write-Host "Writing Breaking Changes"
+
+    $document += "## Breaking Changes`r`n`r`n"
+
+    foreach ($break in $breaks)
+    {
+        $document += WriteContent $break.Type $break.Scope $break.Msg
+    }
+
+    $document += "`r`n"
+}
+
+if ($feats.Length -gt 0)
+{
+    Write-Host "Writing New Features"
+
+    $document += "## New Features`r`n`r`n"
+
+    foreach ($feat in $feats)
+    {
+        $document += WriteContent $feat.Type $feat.Scope $feat.Msg
+    }
+
+    $document += "`r`n"
+}
+
+if ($fixes.Length -gt 0)
+{
+    Write-Host "Writing Bug Fixes"
+
+    $document += "## Bug Fixes`r`n`r`n"
+    
+    foreach ($fix in $fixes)
+    {
+        $document += WriteContent $fix.Type $fix.Scope $fix.Msg
+    }
+
+    $document += "`r`n"
+}
+
+if ($others.Length -gt 0)
+{
+    Write-Host "Writing Other Changes"
+
+    $document += "## Other Changes`r`n`r`n"
+
+    foreach ($other in $others)
+    {
+        $document += WriteContent $other.Type $other.Scope $other.Msg
+    }
+
+    $document += "`r`n"
+}
+
+if ($document -ne "")
+{
+    Write-Host "Generated Changelog."
+    Write-Host $document
+}
+else
+{
+    Write-Error "Unable to generate a changelog."
+    exit 1
+}
