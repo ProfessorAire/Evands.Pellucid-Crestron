4d0195781478477325308a329e099be5b6e22100
ProfessorAire <ProfessorAire@users.noreply.github.com>
2021-07-27 20:29:55 -0700

    chore: Updated workflow badge generation and readme badge urls and revved assemblies to next version.

--- src/Evands.Pellucid/Properties/AssemblyInfo.cs
+++ src/Evands.Pellucid/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 [assembly: AssemblyCompany("Elegant Video and Soundworks")]
 [assembly: AssemblyProduct("Evands.Pellucid")]
 [assembly: AssemblyCopyright("Copyright © Christopher McNeely 2021")]
-[assembly: AssemblyVersion("1.1.3.1000")]
+[assembly: AssemblyVersion("1.2.0.1000")]
 #if Test
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Evands.Pellucid.Tests")]
 #endif
