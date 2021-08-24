11de3cc44b15680c66d965454fb27185e0115649
ProfessorAire <ProfessorAire@users.noreply.github.com>
2021-08-24 15:46:26 -0700

    chore: Started initial work on CWS console feature.

---
+++ src/Evands.Pellucid.CwsConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Evands.Pellucid.CwsConsole")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Evands.Pellucid.CwsConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
