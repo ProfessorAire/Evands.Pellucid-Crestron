58739ff5be3bd203b1d3f313d79cf084558a2fc2
ProfessorAire <ProfessorAire@users.noreply.github.com>
2021-11-06 23:47:50 -0700

    chore: Updated version info in preparation for release.

--- src/Evands.Pellucid.Pro/Properties/AssemblyInfo.cs
+++ src/Evands.Pellucid.Pro/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 [assembly: AssemblyCompany("Elegant Video and Soundworks")]
 [assembly: AssemblyProduct("Evands.Pellucid.Pro")]
 [assembly: AssemblyCopyright("Copyright © Christopher McNeely 2021")]
-[assembly: AssemblyVersion("1.2.2.1000")]
-#if Test
+[assembly: AssemblyVersion("1.3.0.1000")]
+#if TEST
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Evands.Pellucid.Tests")]
 #endif
