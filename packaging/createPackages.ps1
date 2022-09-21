f9ca06c64ff97adf1d5fc2a4d2062a7dd75c3ccd
Christopher McNeely <ProfessorAire@users.noreply.github.com>
2022-09-21 14:17:41 -0400

    chore: Updated packaging steps to build appropriate version.

--- packaging/createPackages.ps1
+++ packaging/createPackages.ps1
@@ -24,7 +24,7 @@ $proPath = "$outPath\Evands.Pellucid.Pro.$proVersion.nupkg"
 
 if ([System.IO.File]::Exists($libPath) -eq $false)
 {
-    Write-Warning "Unable to create nuget package for Evands.Pellucid"
+    Write-Warning "Unable to create nuget package for Evands.Pellucid, unable to find the file: $libPath"
     $exitCode = 1002
 }
 else
@@ -46,14 +46,18 @@ else
 
 if ($exitCode -eq 0)
 {
-    $libDll = "$PSScriptRoot/../src/Evands.Pellucid/bin/Release/Evands.Pellucid.dll"
-    $libXml = "$PSScriptRoot/../src/Evands.Pellucid/bin/Release/Evands.Pellucid.xml"
-    $libProDll = "$PSScriptRoot/../src/Evands.Pellucid.Pro/bin/Release/Evands.Pellucid.Pro.dll"
-    $libProXml = "$PSScriptRoot/../src/Evands.Pellucid.Pro/bin/Release/Evands.Pellucid.Pro.xml"
-    $demo = "$PSScriptRoot/../src/Evands.Pellucid.ProDemo/bin/Release/Evands.Pellucid.ProDemo.cpz"
+    $lib3Dll = "$PSScriptRoot/../src/Series3/Evands.Pellucid/bin/Release/Evands.Pellucid.dll"
+    $lib3Xml = "$PSScriptRoot/../src/Series3/Evands.Pellucid/bin/Release/Evands.Pellucid.xml"
+    $lib3ProDll = "$PSScriptRoot/../src/Series3/Evands.Pellucid.Pro/bin/Release/Evands.Pellucid.Pro.dll"
+    $lib3ProXml = "$PSScriptRoot/../src/Series3/Evands.Pellucid.Pro/bin/Release/Evands.Pellucid.Pro.xml"
+    $lib4Dll = "$PSScriptRoot/../src/Series3/Evands.Pellucid/bin/Release/Evands.Pellucid.dll"
+    $lib4Xml = "$PSScriptRoot/../src/Series3/Evands.Pellucid/bin/Release/Evands.Pellucid.xml"
+    $lib4ProDll = "$PSScriptRoot/../src/Series3/Evands.Pellucid.Pro/bin/Release/Evands.Pellucid.Pro.dll"
+    $lib4ProXml = "$PSScriptRoot/../src/Series3/Evands.Pellucid.Pro/bin/Release/Evands.Pellucid.Pro.xml"
+    $demo = "$PSScriptRoot/../src/Series3/Evands.Pellucid.ProDemo/bin/Release/Evands.Pellucid.ProDemo.cpz"
     
     $archivePath = "$outPath\Evands.Pellucid-Crestron-v$libVersion.zip"
-    Write-Host "Creating release archive."
+    Write-Host "Preparing release archive."
 
     if ([System.IO.File]::Exists($archivePath) -eq $true)
     {
@@ -63,7 +67,45 @@ if ($exitCode -eq 0)
 
     try
     {
-        Compress-Archive $libDll, $libXml, $libProDll, $libProXml, $demo -DestinationPath $archivePath
+        $tempBuildPath = "$PSScriptRoot/.releases/packaging-$libVersion"
+        Write-Host "Temp Build Path = $tempBuildPath"
+        $temp3BuildPath = "$tempBuildPath/Series3"
+        $temp4BuildPath = "$tempBuildPath/Series4"
+
+        if ([System.IO.Directory]::Exists($tempBuildPath) -eq $false)
+        {
+            $dir = [System.IO.Directory]::CreateDirectory($tempBuildPath)
+            Write-Host "Created Directory: $dir"
+        }
+
+        if ([System.IO.Directory]::Exists($temp3BuildPath) -eq $false)
+        {
+            $dir = [System.IO.Directory]::CreateDirectory($temp3BuildPath)
+            Write-Host "Created Directory: $dir"
+        }
+
+        if ([System.IO.Directory]::Exists($temp4BuildPath) -eq $false)
+        {
+            $dir = [System.IO.Directory]::CreateDirectory($temp4BuildPath)
+            Write-Host "Created Directory: $dir"
+        }
+
+        Write-Host "Copying Files"
+
+        Copy-Item $lib3Dll -Destination $temp3BuildPath -Force
+        Copy-Item $lib3Xml -Destination $temp3BuildPath -Force
+        Copy-Item $lib3ProDll -Destination $temp3BuildPath -Force
+        Copy-Item $lib3ProXml -Destination $temp3BuildPath -Force
+        Copy-Item $lib4Dll -Destination $temp4BuildPath -Force
+        Copy-Item $lib4Xml -Destination $temp4BuildPath -Force
+        Copy-Item $lib4ProDll -Destination $temp4BuildPath -Force
+        Copy-Item $lib4ProXml -Destination $temp4BuildPath -Force
+        Copy-Item $demo -Destination $tempBuildPath -Force
+
+        Write-Host "Compressing release archive."
+        Get-ChildItem -Path $tempBuildPath | 
+            Compress-Archive -DestinationPath $archivePath
+            
         Write-Host "Created release archive '$archivePath'."
     }
     catch
