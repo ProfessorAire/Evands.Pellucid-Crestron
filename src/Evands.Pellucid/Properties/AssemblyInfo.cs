841680dfddddca96ac7395da2a3a221f4188382a
ProfessorAire <ProfessorAire@users.noreply.github.com>
2023-06-25 06:56:55 -0700

    chore: Updated version and copyright dates for release.

--- src/Evands.Pellucid/Properties/AssemblyInfo.cs
+++ src/Evands.Pellucid/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@
 [assembly: AssemblyTitle("Evands.Pellucid")]
 [assembly: AssemblyCompany("Elegant Video and Soundworks")]
 [assembly: AssemblyProduct("Evands.Pellucid")]
-[assembly: AssemblyCopyright("Copyright © Christopher McNeely 2022")]
-[assembly: AssemblyVersion("1.5.0.1000")]
+[assembly: AssemblyCopyright("Copyright © Christopher McNeely 2023")]
+[assembly: AssemblyVersion("1.5.1.1000")]
 #if TEST
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Evands.Pellucid.Tests")]
 #endif
